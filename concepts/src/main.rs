d81958914539e8a039a495bfdeae9900ee884426
VSat08 <satyam2001verma@gmail.com>
2024-06-07 10:12:03 +0530

    Concept 1: Comments

---
+++ concepts/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+   println!("#########################");
+   println!("This Programs is for learning the Fundamental Concepts of Rust Programming ");
+   println!("#########################");
+    println!("********** Happy Coding! **********");
+    println!("Comments are implemented using double slash(single line comments) and using star & slash(block comments)");
+    
+    // This is how comment is single line comment is executed
+
+    /*This is how Block comment is executed */
+}
+
